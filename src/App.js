54439edce4c061ea3d2a8d02975764713deb60d6
invector4ik002 <Vedoone@mail.ru>
2021-05-23 17:30:14 +0700

    вроде бы убрал все лишнее

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>project-pizza</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
